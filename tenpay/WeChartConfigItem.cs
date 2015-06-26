81f1ded70a0c89b6908072b2e5fdc1232040da33
dapeng <569305586@qq.com>
2015-06-26 18:09:49 +0800

    6-26

---
+++ tenpay/WeChartConfigItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace tenpay
+{
+    /// <summary>
+    /// 微信支付公共配置项
+    /// </summary>
+    public static  class WeChartConfigItem
+    {
+        /// <summary>
+        /// 微信appid
+        /// </summary>
+        public readonly static string appid = "wx2c2f2e7b5b62daa1";
+
+        /// <summary>
+        /// 商户号
+        /// </summary>
+        public readonly static string mch_id = "1246407101";
+
+        /// <summary>
+        /// 商户key
+        /// </summary>
+        public readonly static string key = "43804496F28A4F0FBF1195AA0F1Abcde";
+    }
+}
